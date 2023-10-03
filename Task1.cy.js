611ecefd40e8bc743a20aea478c70a4ed23e3e07
Jawad Khan <jawadkhan.gg@gmail.com>
2023-10-03 15:46:29 +0500

    3rd commit - commiting all files

--- Task1.cy.js
+++ Task1.cy.js
@@ -11,11 +11,12 @@ describe("Crowlar Tasks" , () => {
 
         cy.get(':nth-child(2) > .element-list > .menu-list > #item-0 > .text').click()
 
-        cy.get('#firstName').type("Cowlar")
-        cy.get('#lastName').type('Developer')
-        cy.get('#userEmail').type('qaengineer@cowlar.com')
+        //Check Assertions and putting values
+        cy.get('#firstName').should('be.visible').type("Cowlar")
+        cy.get('#lastName').should('be.visible').type('Developer')
+        cy.get('#userEmail').should('be.visible').type('qaengineer@cowlar.com')
     
-        //whenever want to check the behavior of element, be means behavior. The assertion to put on Radio
+        //The assertion to put on Radio
         cy.get('#gender-radio-1').should('be.visible')    
         cy.get('#gender-radio-2').should('be.visible')
         cy.get('#gender-radio-3').should('be.visible')
@@ -25,23 +26,25 @@ describe("Crowlar Tasks" , () => {
         cy.get('#gender-radio-2').should('not.be.checked')
         cy.get('#gender-radio-3').should('not.be.checked')
 
-        cy.get('#userNumber').type('0123456789')
-        cy.get('.subjects-auto-complete__value-container').type('Computer Science{enter}')
+        //Check Assertions and putting values
+        cy.get('#userNumber').should('be.visible').type('0123456789')
+        cy.get('.subjects-auto-complete__value-container').should('be.visible').type('Computer Science{enter}')
     
 
-        //checking visiblity of elements
+        //Assertions for checkbox visiblity of checkbox
         cy.get('#hobbies-checkbox-3').should('be.visible')
 
         //selecting check boxes
         cy.get('#hobbies-checkbox-3').check().should('be.checked')
 
-        cy.get('#currentAddress').type('Address 1')
+        cy.get('#currentAddress').should('be.visible').type('Address 1')
 
-        cy.get('#state').click().type('NCR{enter}')
-        cy.get('#city').click().type('Delhi{enter}')
+        cy.get('#state').click().should('be.visible').type('NCR{enter}')
+        cy.get('#city').click().should('be.visible').type('Delhi{enter}')
 
-        cy.get('#submit').click()
+        cy.get('#submit').should('be.visible').click()
 
+        //Assertion for success message
         cy.get('#example-modal-sizes-title-lg').should('have.text','Thanks for submitting the form')
         cy.wait(2000)
         cy.get('#closeLargeModal').click()
